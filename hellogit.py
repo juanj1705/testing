342158551a0b5dfed9c46c275d8eafecea612699
juanj <juanjosecavila@gmail.com>
2024-07-06 15:53:03 -0600

    Version 1

---
+++ hellogit.py
@@ -0,0 +1,27 @@
+print("Hello git!\n");
+# git -h
+# git --help
+print("Configuracion global de git en el equipo local");
+# git config --global user.name "Nombre"
+# git config --global user.email "email@gmail.com"
+print("Cambio de nombre opcional de la rama master a main")
+# git config --global init.defaultBranch main 
+
+print("Crear carpeta .git en carpeta del proyecto");
+# git init
+print("Renombrar la rama si se desea")
+# git branch -m main
+print("Con las excepciones en la carpeta .gitignore");
+# **/ignored.txt
+
+print("Ver el estado actual del git para modificar");
+# git status
+print("Ver el historial del git para modificar");
+# git log
+print("Commandos por default con alias");
+
+print("Añadir fichero o ficheros");
+# git add hellogit.py
+# git add .
+print("Captura de version");
+# git commit -m "Version"
